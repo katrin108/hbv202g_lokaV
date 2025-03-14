8d286371a1b2149ad5062ca7c3d39cc25db5a9f4
katrin108 <kas108@hi.is>
2025-03-14 16:02:17 +0000

    done

--- src/main/java/is/hi/hbv202g/assignment8/Main.java
+++ src/main/java/is/hi/hbv202g/assignment8/Main.java
@@ -1,13 +1,50 @@
 package is.hi.hbv202g.assignment8;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class Main
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws EmptyAuthorListException, UserOrBookDoesNotExistException {
+
+        //create library
         LibrarySystem myLibrarySystem = new LibrarySystem();
+
+        //create new books in the system
+
+        myLibrarySystem.addBookWithTitleAndNameOfSingleAuthor("Narnia", "C. S. Lewis");
+        myLibrarySystem.addBookWithTitleAndNameOfSingleAuthor("hitchiker's guide to the galaxy", "Douglas Adams");
+        myLibrarySystem.addBookWithTitleAndAuthorList("Good omens", List.of(new Author("Terry Pratchett"), new Author("Neil Gaiman")));
+
+        //create new users in the system
+
+        myLibrarySystem.addStudentUser("Ari", true);
+        myLibrarySystem.addStudentUser("Anna", true);
+
+
+        //find books and users in the system
+
+        Book narnia = myLibrarySystem.findBookByTitle("Narnia");
+        User ari = myLibrarySystem.findUserByName("Ari");
+
+
+        //borrow a book
+
+        myLibrarySystem.borrowBook(ari, narnia);
+
+        //extend a lending
+
+        myLibrarySystem.extendLending(new FacultyMember("Andri", "Computer Science"), narnia, LocalDate.now().plusDays(7));
+
+        //return a book
+
+        myLibrarySystem.returnBook(ari, narnia);
+
+
+
     }
 }
---
+++ src/main/java/is/hi/hbv202g/assignment8/UserOrBookDoesNotExistException.java
@@ -0,0 +1,8 @@
+package is.hi.hbv202g.assignment8;
+
+public class UserOrBookDoesNotExistException extends Exception {
+    public UserOrBookDoesNotExistException(String message) {
+        super(message);
+
+    }
+}
